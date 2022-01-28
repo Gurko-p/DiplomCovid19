5f9409e362c089a56566cf5efc43801e19d0a6fd
Pavel <gurko-p@mail.ru>
2022-01-28 17:00:53 +0300

    add Initial data and home controller

--- DiplomCovid19/Migrations/20220128115127_Initial.Designer.cs
+++ DiplomCovid19/Migrations/20220128115127_Initial.Designer.cs
@@ -4,14 +4,16 @@ using DiplomCovid19.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DiplomCovid19.Migrations
 {
     [DbContext(typeof(EmployeeContext))]
-    partial class EmployeeContextModelSnapshot : ModelSnapshot
+    [Migration("20220128115127_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DiplomCovid19/Models/EmployeeContext.cs
+++ DiplomCovid19/Models/EmployeeContext.cs
@@ -14,5 +14,62 @@ namespace DiplomCovid19.Models
         public DbSet<Rank> Ranks { get; set; }
         public DbSet<Subdivision> Subdivisions { get; set; }
         public DbSet<EmployeeVaccineJunction> EmployeeVaccineJunctions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Rank>().HasData(
+                new Rank[]
+                {
+                new Rank { Id=1, RankName="рядовой милиции"},
+                new Rank { Id=2, RankName="младший сержант милиции"},
+                new Rank { Id=3, RankName="сержант милиции"},
+                new Rank { Id=4, RankName="старший сержант милиции"},
+                new Rank { Id=5, RankName="старшина милиции"},
+                new Rank { Id=6, RankName="лейтенант милиции"},
+                new Rank { Id=7, RankName="старший лейтенант милиции"},
+                new Rank { Id=8, RankName="капитан милиции"},
+                new Rank { Id=9, RankName="майор милиции"},
+                new Rank { Id=10, RankName="подполковник милиции"},
+                new Rank { Id=11, RankName="полковник милиции"}
+                });
+
+            modelBuilder.Entity<Position>().HasData(
+                new Position[]
+                {
+                    new Position{ Id = 1, PositionName = "начальник академии" },
+                    new Position{ Id = 2, PositionName = "заместитель начальника академии" },
+                    new Position{ Id = 3, PositionName = "начальник курса" },
+                    new Position{ Id = 4, PositionName = "заместитель начальника курса" },
+                    new Position{ Id = 5, PositionName = "начальник кафедры" },
+                    new Position{ Id = 6, PositionName = "заместитель начальника кафедры" },
+                    new Position{ Id = 7, PositionName = "преподаватель" },
+                    new Position{ Id = 8, PositionName = "преподаватель-методист" },
+                    new Position{ Id = 9, PositionName = "старший преподаватель" },
+                    new Position{ Id = 10, PositionName = "старший преподаватель-методист" },
+                    new Position{ Id = 11, PositionName = "старший инспектор" },
+                    new Position{ Id = 12, PositionName = "старший инспектор по особым поручениям" },
+                    new Position{ Id = 13, PositionName = "доцент" },
+                    new Position{ Id = 14, PositionName = "профессор" },
+                    new Position{ Id = 15, PositionName = "курсант" }
+                });
+            modelBuilder.Entity<Subdivision>().HasData(
+                new Subdivision[]
+                {
+                    new Subdivision{ Id = 1, SubdivisionName = "отдел кадров"},
+                    new Subdivision{ Id = 2, SubdivisionName = "отдел образовательных информационных технологий"},
+                    new Subdivision{ Id = 3, SubdivisionName = "учебно-методическое управление"}
+                });
+            modelBuilder.Entity<Employee>().HasData(
+                    new Employee[]
+                    {
+                        new Employee{ Id = 1, FIO = "Гурко Павел Михайлович", SubdivisionId = 2, PositionId = 8, RankId = 9 },
+                        new Employee{ Id = 2, FIO = "Левенко Евгений Юрьевич", SubdivisionId = 2, PositionId = 10, RankId = 10 },
+                        new Employee{ Id = 3, FIO = "Гуркский Вадим Михайлович", SubdivisionId = 2, PositionId = 10, RankId = 10 },
+                        new Employee{ Id = 4, FIO = "Гейц Людмила Николаевна", SubdivisionId = 1, PositionId = 11, RankId = 8 },
+                        new Employee{ Id = 5, FIO = "Костян Елена Григорьевна", SubdivisionId = 1, PositionId = 11, RankId = 8 },
+                        new Employee{ Id = 6, FIO = "Бедункевич Марина Александровна", SubdivisionId = 1, PositionId = 12, RankId = 10 },
+                        new Employee{ Id = 7, FIO = "Райкова Екатерина Александровна", SubdivisionId = 3, PositionId = 10, RankId = 10 }
+                    });
+        }
     }
 }
